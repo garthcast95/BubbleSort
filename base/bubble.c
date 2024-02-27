821308b6f5bb400bed550129874d554ee0f441b4
garthcast95 <161513958+garthcast95@users.noreply.github.com>
2024-02-27 14:32:48 -0500

    Add files via upload
    Bubble Sort Algoritm

---
+++ base/bubble.c
@@ -0,0 +1,36 @@
+//bubble.c
+///famous bubble sort
+//implement the swap algorithm with pointers
+
+#include <stdio.h>
+#define MAX 9
+//function prototypes
+void printValues(int Values[]){
+printf(Values[])
+	
+
+ 
+void sort(int values[], int n)
+{ int i, j;
+for (i = 0; i < n - 1; i++)
+	for i in list - 1
+		for j in list - 1
+			if list[j] > list[j+1]
+				swap list[j] and list [j+1]
+}
+
+
+
+void swap(int*, int*);
+
+int values[] = {7, 3, 9, 4, 6, 1, 2, 8, 5};
+
+int main(){
+	printf("Before: \n");
+	printValues();
+	sort();
+	printf("After: \n");
+	printValues();
+return(0);
+}
+//end main
